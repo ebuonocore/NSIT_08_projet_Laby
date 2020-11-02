da5f986cfd58faf9f997232038c7b910e26ffb64
ebuonocore <45919390+ebuonocore@users.noreply.github.com>
2020-11-02 19:36:47 +0100

    Add files via upload

---
+++ Sources_eleves/creation_laby.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#importation des bibliothèques
+
+
+def creer_labyrinthe():
+    """ 
+    """
+    
+    
+    
+if __name__ == "__main__":
+    # Lance le test de la fonction creer_labyrinthe()
+    Labyrinthe = creer_labyrinthe(4, 3, 30)
+    m = nx.adjacency_matrix(Labyrinthe)
+    print(m)
